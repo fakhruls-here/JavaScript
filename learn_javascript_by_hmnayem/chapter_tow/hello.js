ff95d5b13ee8472fb4dbe92df3f5d1b9f9ac3940
fakhruls-here <fakhrulishere@gmail.com>
2023-04-15 06:12:16 -0700

    New something learn here

---
+++ learn_javascript_by_hmnayem/chapter_tow/hello.js
@@ -0,0 +1,7 @@
+console.log('hello world')  //this is a String data type
+console.log("Hello world")  //this is a String data type
+console.log("'FAKHRUL ISLAM, You are Welcome to the JavaScript world'") //this is a String data type
+console.log(1234567890) //this is a Number data type
+console.log('My age ' + 23) //this is a String with Number data type
+console.log(0.1738922671)   //this is a Floating Number data type
+console.log('HelloBy- ', 0.1738922671)  //this is a String with Floating Number data type
