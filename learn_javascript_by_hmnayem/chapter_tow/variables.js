f06d5adf2a25abe1c18733e8c35618a4623bd13a
fakhruls-here <fakhrulishere@gmail.com>
2023-04-16 04:09:08 -0700

    today learned something new knowledge in JS

---
+++ learn_javascript_by_hmnayem/chapter_tow/variables.js
@@ -0,0 +1,30 @@
+// Step-1.
+// Remember you have some data that the define in the console.log()
+
+console.log('FI Miraj have learn JavaScript')
+console.log('His age is only 24')
+console.log('FI Miraj learning a JavaScript Course All You Need to Know')
+console.log('But his age is only 24')
+
+
+// Step-2.
+// Remember you have some data that the define in the console.log(), but now you need to change your some information in the console.log(), 
+// thats simple change this information bcz of less info. 
+
+console.log('FI Miraj have learn JavaScript')
+console.log('His age is only 24')
+console.log('FI Miraj learning a JavaScript Course All You Need to Know')
+console.log('But his age is only 24')
+
+
+// Step-3.
+// Remember you have some data that the define in the console.log(), but now you need to change your some information in the console.log(), thats simple change this information bcz of less info, 
+// If you have a lot of data list? what will you can do??
+// You can use Variable for the data Store, then you can change any data any how and any where, like this under the below
+
+var fName = 'FAKHRUL ISLAM'
+var age  = '23 years old'
+console.log( fName + ' Miraj have learn JavaScript')
+console.log('His age is only ' + age)
+console.log( fName + ' Miraj learning a JavaScript Course All You Need to Know')
+console.log('But his age is only ' + age)
