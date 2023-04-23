d4fb225fef93e6f09f03c6811c097c0a24b1b51d
fakhruls-here <fakhrulishere@gmail.com>
2023-04-22 21:13:58 -0700

    i have learn today new something

---
+++ learn_javascript_by_hmnayem/chapter_tow/dataTypes.js
@@ -0,0 +1,18 @@
+var thatsCalledNumber0 = 12;
+var thatsCalledNumber1 =  100.34;
+var thatsCalledString0 = 'hello javaScript single comma';
+var thatsCalledString1 = "hello javaScript double comma";
+var thatsCalledString2 = `hello javaScript backTick`;
+var thatsCalledBoolean0 = true;
+var thatsCalledBoolean1 = false;
+var thatsCalledNull = null;
+var thatsCalledUndefined = 
+console.log(thatsCalledNumber0)
+console.log(thatsCalledNumber1)
+console.log(thatsCalledString0)
+console.log(thatsCalledString1)
+console.log(thatsCalledString2)
+console.log(thatsCalledBoolean0)
+console.log(thatsCalledBoolean1)
+console.log(thatsCalledUndefined)
+console.log(thatsCalledNull)
