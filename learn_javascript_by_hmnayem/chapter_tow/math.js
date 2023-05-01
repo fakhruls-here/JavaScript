4b3b2f843f715cc6b5747a223f768dcc117f3840
fakhruls-here <fakhrulishere@gmail.com>
2023-05-01 23:31:38 +0600

    this is new comment here

---
+++ learn_javascript_by_hmnayem/chapter_tow/math.js
@@ -0,0 +1,19 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 4.434
+console.log(Math.abs(n))        // Math.abs means variable in stored value is abs/exact value
+console.log(Math.floor(n))      // Math.floor means main value in (.) afterValue dotted is under dotted agosValue shown 
+console.log(Math.ceil(n))       // Math.ceil means main value is always to upstream like 4.634==5; 
+console.log(Math.round(n))
+
+console.log(Math.max(283,34, 239,2352))
+console.log(Math.min(57, 834, 3848, 838, 838, 3))
+
+console.log(Math.pow(2,3))
+console.log(Math.pow(4,2,6))
+
+console.log(Math.sqrt(9))
+console.log(Math.sqrt(49))
+
+console.log(Math.random())
---
+++ learn_javascript_by_hmnayem/chapter_tow/statements.js
@@ -0,0 +1,11 @@
+// simple statement 
+/** there are simple statements for js 
+ * console.log('hello world')
+ * var str = "miraj"
+ * var number = 32 + (44/4) * 6
+ * 
+ * var a = 20
+ * var b = 40
+ * var c = a>=b
+ * 
+ */
